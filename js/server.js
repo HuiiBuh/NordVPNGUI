fd60cd734c09c649a623b2038ebc4170212ef1c2
HuiiBuh <nhaderer1@gmail.com>
2019-02-12 22:44:55 +0100

    Improvements
    Made all kinds of improvements and beautified the code in all modified files.
    Added functions in myConnection.js so you can quickconnect to a country. However this is only a test and will be better implemented in a later version on this project

--- js/server.js
+++ js/server.js
@@ -1,101 +1,74 @@
+//executed once when the page gets loaded
 $(document).ready(function () {
 
-  doneResizing();
+    positionElements();
 
-  $("#allCountries").collapse('show')
+    $("#allCountries").on('show.bs.collapse', function () {
+        document.getElementById("allCountriesIcon").innerHTML = "keyboard_arrow_down";
+    });
+    $("#allCountries").on('hide.bs.collapse', function () {
+        document.getElementById("allCountriesIcon").innerHTML = "keyboard_arrow_right";
+    });
 
-  $("#allCountries").on('show.bs.collapse', function () {
-    document.getElementById("allCountriesIcon").innerHTML = "keyboard_arrow_down";
-  });
-  $("#allCountries").on('hide.bs.collapse', function () {
-    document.getElementById("allCountriesIcon").innerHTML = "keyboard_arrow_right";
-  });
+    $("#specialServers").on('show.bs.collapse', function () {
+        document.getElementById("specialServerIcon").innerHTML = "keyboard_arrow_down";
+    });
+    $("#specialServers").on('hide.bs.collapse', function () {
+        document.getElementById("specialServerIcon").innerHTML = "keyboard_arrow_right";
+    });
 
-  $("#specialServers").on('show.bs.collapse', function () {
-    document.getElementById("specialServerIcon").innerHTML = "keyboard_arrow_down";
-  });
-  $("#specialServers").on('hide.bs.collapse', function () {
-    document.getElementById("specialServerIcon").innerHTML = "keyboard_arrow_right";
-  });
+    $("#allCountries").collapse('show');
+
+
+    let worker = new Worker("js/connectionChecker.js");
+    worker.postMessage("Run");
+
+
 });
 
-var resizeId;
-$(window).resize(function () {
-  clearTimeout(resizeId);
-  resizeId = setTimeout(doneResizing, 1);
-});
+function positionElements() {
+    //set the mainpage to the right height
+    let body = window.innerHeight;
+    let navbar = document.getElementById('navbar').offsetHeight;
+
+    let mainPageHeight = body - navbar;
+    document.getElementById("mainPage").style.height = (mainPageHeight).toString() + "px";
+
+    //set the floating connect Button to the right position
+    let mapWidth = document.getElementById("map").offsetWidth;
+    let connectionStatusWidth = mapWidth * 2 / 3;
+
+    let connectionStatusBox = document.getElementById("connectionStatusBox");
 
 
-function doneResizing() {
-  //set the mainpage to the right height
-  var body = window.innerHeight;
-  var navbar = document.getElementById('navbar').offsetHeight;
-
-  var mainPageHeight = body - navbar;
-  document.getElementById("mainPage").style.height = (mainPageHeight).toString() + "px";
-
-  //set the floating connect Button to the right position
-  mapWidth = document.getElementById("map").offsetWidth;
-  connectionStatusWidth = mapWidth * 2 / 3;
-
-  connectionStatusBox = document.getElementById("connectionStatusBox");
+    if (mapWidth > 575) {
+        connectionStatusBox.style.bottom = (mainPageHeight / 40).toString() + "px";
+        connectionStatusBox.style.right = (mapWidth / 6).toString() + "px";
+        connectionStatusBox.style.width = (connectionStatusWidth).toString() + "px";
+    } else {
+        connectionStatusBox.style.bottom = (mainPageHeight / 40).toString() + "px";
+        connectionStatusBox.style.right = "15px";
+        connectionStatusBox.style.width = (mapWidth).toString() + "px";
+    }
 
 
-  if (mapWidth > 575) {
-    connectionStatusBox.style.width = (connectionStatusWidth).toString() + "px";
-    connectionStatusBox.style.right = (mapWidth * 1 / 6).toString() + "px";
-    connectionStatusBox.style.bottom = (mainPageHeight * 1 / 40).toString() + "px";
-  }
-  else {
-    connectionStatusBox.style.width = (mapWidth).toString() + "px";
-    connectionStatusBox.style.right = "15px";
-    connectionStatusBox.style.bottom = (mainPageHeight * 1 / 40).toString() + "px";
-  }
-
-  connectionStatus = document.getElementById("connectionStatus");
-  connectionStatusHight = connectionStatus.offsetHeight;
-  if (connectionStatusHight > 63) {
-    connectionStatus.style.paddingTop = "0px"
-  }
-  else if (connectionStatusHight == 60) {
-    connectionStatus.style.paddingTop = "15px"
-  }
+    //TODO Adjust the padding of the Textbox in the connectionStatus box
+    /*
+    connectionStatus = document.getElementById("connectionStatus");
+    connectionStatusHight = connectionStatus.offsetHeight;
+    if (connectionStatusHight > 63) {
+      connectionStatus.style.paddingTop = "0px"
+    }
+    else if (connectionStatusHight === 60) {
+      connectionStatus.style.paddingTop = "15px"
+    }
+    */
 }
 
 
-/*
-
-$(function() {
-  //add BT DD show event
-  $(".dropdown").on("show.bs.dropdown", function() {
-    var $btnDropDown = $(this).find(".dropdown-toggle");
-    var $listHolder = $(this).find(".dropdown-menu");
-    //reset position property for DD container
-    $(this).css("position", "static");
-    $listHolder.css({
-      "top": ($btnDropDown.offset().top + $btnDropDown.outerHeight(true)) + "px",
-      "left": $btnDropDown.offset().left + "px"
-    });
-    $listHolder.data("open", true);
-  });
-  //add BT DD hide event
-  $(".dropdown").on("hidden.bs.dropdown", function() {
-    var $listHolder = $(this).find(".dropdown-menu");
-    $listHolder.data("open", false);
-  });
-  //add on scroll for table holder
-  $(".ak-holder").scroll(function() {
-    var $ddHolder = $(this).find(".dropdown")
-    var $btnDropDown = $(this).find(".dropdown-toggle");
-    var $listHolder = $(this).find(".dropdown-menu");
-    if ($listHolder.data("open")) {
-      $listHolder.css({
-        "top": ($btnDropDown.offset().top + $btnDropDown.outerHeight(true)) + "px",
-        "left": $btnDropDown.offset().left + "px"
-      });
-      $ddHolder.toggleClass("open", ($btnDropDown.offset().left > $(this).offset().left))
-    }
-  })
+//calls the positionElements() 5ms after a resizeEvent (if no other resize event has occurred)
+let resizeId;
+$(window).resize(function () {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(positionElements, 5);
 });
-
-*/
--- js/settings.js
+++ js/settings.js
@@ -1,40 +1,35 @@
 function checkKey(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    saveDNS();
-  }
-  event.stopPropagation();
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        saveDNS();
+    }
+    event.stopPropagation();
 }
 
 function saveDNS() {
-  var DNS = document.getElementById("setcustomDNS").value;
+    let DNS = document.getElementById("setcustomDNS").value;
 
-  if (/^\d\d?\d?\d?\.\d\d?\d?\d?\.\d\d?\d?\d?\.\d\d?\d?\d?$/.test(DNS)) {
-    var temp = document.getElementById("customDNSBadge")
-    if (temp != null) {
-      if (DNS != temp.innerHTML) {
-        temp.innerHTML = DNS
-      }
-    }
-    else {
-      document.getElementById("DNSModalTrigger").innerHTML = 'Change DNS';
-      var badge = document.createElement("span");
-      badge.className = "badge badge-secondary"
-      badge.id = "customDNSBadge";
-      badge.style.marginRight = "5px"
-      var customDNS = document.createTextNode(DNS);
-      badge.appendChild(customDNS);
-      var element = document.getElementById("customDNSText");
-      var child = document.getElementById("DNSModalTrigger");
-      element.insertBefore(badge, child);
-    }
+    if (/^\d\d?\d?\d?\.\d\d?\d?\d?\.\d\d?\d?\d?\.\d\d?\d?\d?$/.test(DNS)) {
+        let temp = document.getElementById("customDNSBadge");
+        if (temp != null) {
+            if (DNS != temp.innerHTML) {
+                temp.innerHTML = DNS
+            }
+        } else {
+            document.getElementById("DNSModalTrigger").innerHTML = 'Change DNS';
+            let badge = document.createElement("span");
+            badge.className = "badge badge-secondary";
+            badge.id = "customDNSBadge";
+            badge.style.marginRight = "5px";
+            let customDNS = document.createTextNode(DNS);
+            badge.appendChild(customDNS);
+            let element = document.getElementById("customDNSText");
+            let child = document.getElementById("DNSModalTrigger");
+            element.insertBefore(badge, child);
+        }
 
-    $("#DNSChange").modal("hide")
-  }
-  else {
-    alert("Your DNS has not the right format.")
-  }
+        $("#DNSChange").modal("hide")
+    } else {
+        alert("Your DNS has not the right format.")
+    }
 }
-/*
-badge badge-secondary
-*/
